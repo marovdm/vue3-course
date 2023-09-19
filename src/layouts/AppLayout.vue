99d66203d7b4cf1bfa5e5005a354e41e62c4beeb
Dmitrii Marov <marovdm.art@gmail.com>
2023-09-19 21:28:06 +0300

    inital commit, base layout app

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <app-header />
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import AppHeader from "./AppHeader.vue";
+</script>
+
+<style lang="scss" scoped>
+.app_layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+</style>
