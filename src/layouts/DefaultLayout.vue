4f2e9ccad29d0b9fd8194b216d54688e2892ae8a
Dmitrii Marov <marovdm.art@gmail.com>
2023-11-30 15:32:10 +0300

    роутер и маршруты приложения

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="default-layout">
+    <default-header />
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import DefaultHeader from "./DefaultHeader.vue";
+</script>
+
+<style lang="scss" scoped>
+.app_layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+</style>
